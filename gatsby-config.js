b60ba18a6589310755778a96290ca1b15c326bc1
Dushyant Priyadarshee <dushyant.priyadarshee@sharp-ls.eu>
2019-10-18 20:47:53 +0100

    Customize settings. These are included in rss generation.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'RubberDuckDev',
+    description: 'Blog posts by Dushyant Priyadarshee',
+    siteUrl: 'https://rubberduckdev.com', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
